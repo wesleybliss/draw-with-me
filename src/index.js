7ca8e49ea8bd799d322b60c9892bab0dd914c6f2
Wesley Bliss <wesley.bliss@gmail.com>
2017-11-24 22:38:28 -0500

    Organized styles into directories

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();*/
-
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
