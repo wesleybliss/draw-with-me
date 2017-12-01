cca4225c6992317dea21e3b326d0a8af7f79f1d4
Wesley Bliss <wesley.bliss@gmail.com>
2017-11-30 21:45:29 -0500

    Added Pug to project, but need to eject for it to respect Babel config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
+/* eslint-disable */
+
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './redux/store'
 import App from './containers/app'
+import template from './index.pug'
 import registerServiceWorker from './registerServiceWorker';
 
 import './css/index.css'
 
 const target = document.querySelector('#root')
 
-render(
-    <Provider store={store}>
+/*
+<Provider store={store}>
         <ConnectedRouter history={history}>
             <div>
                 <App />
             </div>
         </ConnectedRouter>
-    </Provider>,
+    </Provider>
+*/
+
+render(
+    pug`
+        Provider(store = store)
+            ConnectedRouter(history = history)
+                div
+                    App
+    `,
     target
 )
 
