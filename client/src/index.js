00a78cb48b83e14a022421102c67a889ca0fb404
Wesley Bliss <wesley.bliss@gmail.com>
2017-12-02 03:09:33 -0500

    Replaced create-react-app with NWB

--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,9 @@
-/* eslint-disable */
+import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
 
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './redux/store'
-import App from './containers/app'
-import template from './index.pug'
-import registerServiceWorker from './registerServiceWorker';
 
-import './css/index.css'
+import App from './App'
 
-const target = document.querySelector('#root')
-
-/*
-<Provider store={store}>
-        <ConnectedRouter history={history}>
-            <div>
-                <App />
-            </div>
-        </ConnectedRouter>
-    </Provider>
-*/
-
-render(
-    pug`
-        Provider(store = store)
-            ConnectedRouter(history = history)
-                div
-                    App
-    `,
-    target
-)
-
-registerServiceWorker()
+render(<App/>, document.querySelector('#app'))
