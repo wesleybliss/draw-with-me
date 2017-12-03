c10f8feec244be9d88b37740f86ed02731bd2642
Wesley Bliss <wesley.bliss@gmail.com>
2017-12-03 03:35:14 -0500

    Added real links to navbar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -2,19 +2,19 @@ import React, {Component} from 'react'
 import { Route, Link } from 'react-router-dom'
 
 const template = pug`
-    nav.navbar.navbar-expand-lg.navbar-light.bg-light
-        a.navbar-brand(href="#") Navbar
+    nav.navbar.navbar-expand-lg.navbar-dark.bg-dark
+        a.navbar-brand(href="#") DWM
         button.navbar-toggler(type="button", data-toggle="collapse", data-target="#navbarSupportedContent", aria-controls="navbarSupportedContent", aria-expanded="false", aria-label="Toggle navigation")
             span.navbar-toggler-icon
         #navbarSupportedContent.collapse.navbar-collapse
             ul.navbar-nav.mr-auto
                 li.nav-item.active
-                    a.nav-link(href="#")
+                    Link.nav-link(to="/")
                         | Home 
                         span.sr-only (current)
                 li.nav-item
-                    a.nav-link(href="#") Link
-                li.nav-item.dropdown
+                    Link.nav-link(to="/chat") Chat
+                //- li.nav-item.dropdown
                     a#navbarDropdown.nav-link.dropdown-toggle(href="#", role="button", data-toggle="dropdown", aria-haspopup="true", aria-expanded="false")
                         | Dropdown
                     .dropdown-menu(aria-labelledby="navbarDropdown")
@@ -22,9 +22,9 @@ const template = pug`
                         a.dropdown-item(href="#") Another action
                         .dropdown-divider
                         a.dropdown-item(href="#") Something else here
-                li.nav-item
+                //- li.nav-item
                     a.nav-link.disabled(href="#") Disabled
-            form.form-inline.my-2.my-lg-0
+            //- form.form-inline.my-2.my-lg-0
                 input.form-control.mr-sm-2(type="search", placeholder="Search", aria-label="Search")
                 button.btn.btn-outline-success.my-2.my-sm-0(type="submit") Search
 `
