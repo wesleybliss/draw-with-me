4cfc642675b0429f19a3341fdcd3919b80f663aa
Wesley Bliss <wesley.bliss@gmail.com>
2017-12-03 04:17:48 -0500

    Added basic working client-side drawing capability

--- client/src/pages/draw/index.js
+++ client/src/pages/draw/index.js
@@ -1,4 +1,4 @@
-import '../../styles/chat.styl'
+import '../../styles/draw.styl'
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
@@ -24,11 +24,55 @@ class Draw extends Component {
     displayName: 'Draw'
     
     state = {
-        foo: ''
+        canvas: null,
+        ctx: null,
+        drawing: false
+    }
+    
+    // bottom, height, left, right, top, width, x, y
+    getBounds = () => this.refs.canvas.getBoundingClientRect()
+    
+    getDrawPoint = e => {
+        const bounds = this.getBounds()
+        const x = e.clientX - bounds.x
+        const y = e.clientY - bounds.y
+        return { x, y }
+    }
+    
+    handleMouseDown = e => {
+        e.preventDefault()
+        const { x, y } = this.getDrawPoint(e)
+        this.state.ctx.moveTo(x, y)
+        this.state.drawing = true
+    }
+    
+    handleMouseUp = e => {
+        e.preventDefault()
+        this.state.drawing = false
+    }
+    
+    handleMouseMove = e => {
+        
+        // Don't draw unless mouse is down
+        if (!this.state.drawing) return
+        
+        const { ctx } = this.state
+        const { x, y } = this.getDrawPoint(e)
+        
+        ctx.lineTo(x, y)
+        ctx.stroke()
+        
     }
     
     componentDidMount() {
         
+        // this.state.canvas = document.querySelector('#canvas')
+        const ctx = this.refs.canvas.getContext('2d')
+        this.state.ctx = ctx
+        
+        ctx.fillStyle = 'rgb(0, 0, 0)'
+        
+        console.log('this.refs.canvas.offsetTop', this.refs.canvas.offsetTop)
         
     }
     
@@ -40,7 +84,15 @@ class Draw extends Component {
                     h1 Draw
             .row
                 .col
-                    p Hi
+                    canvas(
+                        ref = "canvas",
+                        id = "canvas",
+                        width = "800",
+                        height = "600",
+                        onMouseDown = this.handleMouseDown,
+                        onMouseUp = this.handleMouseUp,
+                        onMouseMove = this.handleMouseMove
+                    )
             
             `
         
